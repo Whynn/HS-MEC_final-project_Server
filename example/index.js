72f926b05c52c6f4e97b2404dc802f00db116378
Hwa In Kim <fine612@gmail.com>
2016-05-05 09:05:35 +0000

    edit index.js and install node-dht-sensor module

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var sensorLib = require("node-dht-sensor");
 
 app.listen(process.env.PORT, process.env.IP, 0, function () {
   console.log('Example app listening!');
@@ -17,3 +18,25 @@ app.get('/', function (req, res) {
   });
   //res.send('Hello World!');
 });
+
+var sensor = {
+    initialize: function() {
+        return sensorLib.initialize(22, 4);
+    },
+    read: function() {
+        var readout = sensorLib.read();
+        var temp = readout.temperature.toFixed(2);
+        var humid = readout.humidity.toFixed(2);
+        
+        console.log('Temperature: ' + temp + 'C, ' + 'Humidity: ' + humid + '%');
+        setTimeout(function () {
+            sensor.read();
+        }, 2000);
+    }
+};
+
+if (sensor.initialize()){
+    sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
