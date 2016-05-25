d8277670ca0ae1c2f34bf6b267fb18f730c55f95
Whynn <fine612@gmail.com>
2016-05-25 06:20:24 +0000

    almost complete

---
+++ example/flaskapp/app/dustmodule.c
@@ -0,0 +1,40 @@
+#include "./iolib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <python.h>
+
+#define DUST_PORT 8
+#define DUST_PIN 12   // P8_12
+#define SAMPLETIME_MS 30000.0
+
+double lowOccupancy = 0; // in ms
+double fallingStart = 0; // in us
+
+void edge_detected(int gpio, int level, double tick) {
+    if(0 == level):
+        fallingStart = tick;
+    if(1 == level && fallingStart != 0):
+        lowOccupancy += (tick - fallingStart) / 1000; //reduce resolution to ms
+}
+
+int main() {
+    if(iolib_init() < 0): return 1; // init pigpio lib
+    if(iolib_setdir(DUST_PORT, DUST_PIN, DIR_IN) < 0): return 1; // configure DUST_PIN as input
+//    if(gpioSetAlertFunc(DUST_PIN, edge_detected) < 0) return 1; // configure interrupt routine for DUST_PIN
+//    if(gpioDelay(SAMPLETIME_MS * 1000) < 0) return 1; // wait SAMPLETIME_MS (gpioDelay wants us)
+//    gpioTerminate(); // try to terminate pigpio lib
+
+    float ratio = (lowOccupancy / SAMPLETIME_MS) * 100; // calculate low occupancy in percent
+
+  /*
+  calculate particle concentration
+  -> amount of particles per 283ml | particle size > 1um
+  eqation for calculation from http://www.howmuchsnow.com/arduino/airquality/grovedust/
+  */
+    float concentration = 1.1 * ratio * ratio * ratio - 3.8 * ratio * ratio + 520 * ratio + 0.62;
+
+  //printf("%d %f %f", lowOccupancy, ratio, concentration);
+    printf("%.1f", ratio);
+    return 0;
+}
--- example/flaskapp/app/routes.py
+++ example/flaskapp/app/routes.py
@@ -4,77 +4,120 @@ sys.path.append('/var/lib/cloud9/server/HS-MEC_final-project_Server/example/am23
 from flask import Flask, render_template
 import Adafruit_DHT
 import serial
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
 
 app = Flask(__name__)
+ADC.setup()
 HTsensor = Adafruit_DHT.AM2302
-HTpin = 'P8_11'
-ser = serial.Serial('/dev/ttyACM0', 9600)
+HTPin = 'P8_7'
+DPin = 'P8_10'
+hitterPin = 'P8_8'
+GPIO.setup(hitterPin, GPIO.OUT)
+airconPin1= 'P8_16'
+airconPin2= 'P8_18'
+GPIO.setup(airconPin1, GPIO.OUT)
+GPIO.setup(airconPin2, GPIO.OUT)
+humidifierPin1 = 'P9_23'
+humidifierPin2 = 'P9_25'
+GPIO.setup(humidifierPin1, GPIO.OUT)
+GPIO.setup(humidifierPin2, GPIO.OUT)
+coolerPin = 'P8_12'
+GPIO.setup(coolerPin, GPIO.OUT)
+humidity = 0
+temperature = 0
+dustVal = 0
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(HTsensor, HTpin)
-    dustVal = ser.readline()
-    if dustVal is not None:
-        print(dustVal+"%")
-    else:
-        print("Failed to get Dust Sensor value.")
+def read():
+    global humidity, temperature, dustVal
+    humidity, temperature = Adafruit_DHT.read_retry(HTsensor, HTPin)
+    #dustRatio = ser.readline()
+    #dustVal = float(dustRatio[:4])
+    #if dustVal is not None:
+    #    print "%0.1f" % dustVal
+    #else:
+    #    print("Failed to get Dust Sensor value.")
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         print('Failed to get reading. Try again!')
-    time.sleep(1)
 
 @app.route('/')
 def home():
     return render_template('index.html')
     
-@app.route('/temp', method=['POST', 'GET'])
+@app.route('/temp/', methods=['POST', 'GET'])
 def readTemp():
     #192.168.7.2:1234/temp
+    read()
     if temperature is not None:
-        return '%d' % (temperature)
+        return '%0.1f' % (temperature)
     else:
         return 'Failed.'
 
-@app.route('/humid', method=['POST', 'GET'])
+@app.route('/humid/', methods=['POST', 'GET'])
 def readHumid():
     #192.168.7.2:1234/humid
     if humidity is not None:
-        return '%d' % (humidity)
+        return '%0.1f' % (humidity)
     else:
         return 'Failed.'
 
-@app.route('/dust', method=['POST', 'GET'])
+@app.route('/dust/', methods=['POST', 'GET'])
 def readDust():
     #192.168.7.2:1234/dust
-    return '%d' % (aread)
-    
+    if dustVal is not None:
+        return '%0.1f' % (dustVal)
+    else:
+        return 'Failed.'
     
 
-@app.route('/hitter/<int:status>', method=['POST', 'GET'])
+@app.route('/hitter/<int:status>', methods=['POST', 'GET'])
 def hitterOnOff(status):
     #192.168.7.2:1234/hitter/0 -> hitter Off
-    #192.168.7.2:1234/hitter/255 -> hitter On
+    #192.168.7.2:1234/hitter/1 -> hitter On
+    if status == 1:
+        GPIO.output('P8_8', GPIO.HIGH)
+    elif status == 0:
+        GPIO.output('P8_8', GPIO.LOW)
     return '%d' % (status)
     
-@app.route('/aircon/<int:status>', method=['POST', 'GET'])
+@app.route('/aircon/<int:status>', methods=['POST', 'GET'])
 def airconOnOff(status):
     #192.168.7.2:1234/aircon/0 -> aircon Off
-    #192.168.7.2:1234/aircon/255 -> aircon On
+    #192.168.7.2:1234/aircon/1 -> aircon On
+    if status == 1:
+        GPIO.output('P8_16', GPIO.HIGH)
+        GPIO.output('P8_18', GPIO.HIGH)
+    elif status == 0:
+        GPIO.output('P8_16', GPIO.LOW)
+        GPIO.output('P8_18', GPIO.LOW)
     return '%d' % (status)
     
 
-@app.route('/humidifier/<int:status>', method=['POST', 'GET'])
+@app.route('/humidifier/<int:status>', methods=['POST', 'GET'])
 def humidifierOnOff(status):
     #192.168.7.2:1234/humidifier/0 -> humidifier Off
-    #192.168.7.2:1234/humidifier/255 -> humidifier On
+    #192.168.7.2:1234/humidifier/1 -> humidifier On
+    if status == 1:
+        GPIO.output('P9_23', GPIO.HIGH)
+        GPIO.output('P9_25', GPIO.HIGH)
+    elif status == 0:
+        GPIO.output('P9_23', GPIO.LOW)
+        GPIO.output('P9_25', GPIO.LOW)
     return '%d' % (status)
    
-@app.route('/cooler/<int:status>', method=['POST', 'GET'])
+@app.route('/cooler/<int:status>', methods=['POST', 'GET'])
 def coolerOnOff(status):
     #192.168.7.2:1234/cooler/0 -> cooler Off
-    #192.168.7.2:1234/cooler/255 -> cooler On
+    #192.168.7.2:1234/cooler/1 -> cooler On
+    if status == 1:
+        GPIO.output('P8_12', GPIO.HIGH)
+    elif status == 0:
+        GPIO.output('P8_12', GPIO.LOW)
     return '%d' % (status)
    
 if __name__ == '__main__':
     app.debug=True
-    app.run(host)
+    app.run(host='0.0.0.0')
